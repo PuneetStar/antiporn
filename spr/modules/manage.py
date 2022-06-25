9b6cd72f71c0b5dac6f369b997667da7ac9ec71d
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2022-06-25 19:16:23 +0530

    Update manage.py

--- spr/modules/manage.py
+++ spr/modules/manage.py
@@ -143,7 +143,7 @@ async def scanNLP(_, message: Message):
     data = await arq.nlp(text)
     data = data.results[0]
     msg = f"""
-**Ham:** {results.ham}
+**Ham:** {data.ham}
 **Is_Spam:** {data.is_spam} %
 **Spam:** {data.spam}
 **Spam Probability:** {data.spam_probability} %
